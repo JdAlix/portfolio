5af7269825dc9256042b6676cc604a8a949cddc9
Alix JEUDI--LEMOINE <alix.jeudi--lemoine@etu.uca.fr>
2024-04-06 17:22:41 +0200

    Ajout CV + lien vers téléchargement (prise en compte thème pour flèche dark/light

--- portfolio/src/views/CvView.vue
+++ portfolio/src/views/CvView.vue
@@ -1,17 +1,81 @@
 <template>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 offset-md-2">
-          <h2 class="mb-4">Mon CV</h2>
-          <p class="lead">INSÉRER CV.</p>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h2 class="mb-4 text-center">Mon CV</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <!-- Image du CV -->
+        <div class="position-relative">
+          <!-- Flèche à gauche -->
+          <div class="arrow left-arrow">
+            <img :src="arrowImage" alt="flèche" srcset="">
+          </div>
+
+          <!-- Image du CV -->
+          <a :href="cv" download="CV_Alix_JEUDI.pdf">
+            <img src="@/assets/CV.jpg" alt="CV de Alix JEUDI" class="w-100">
+          </a>
+          
+          <p class="lead">Cliquez sur le CV pour le télécharger en PDF.</p>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'CvView',
-    }
-</script>
+import arrowWhite from '@/assets/arrow_white.png';
+import arrowDark from '@/assets/arrow.png';
+import cv from '@/assets/CV.pdf';
+
+export default {
   
+  name: 'CvView',
+  data() {
+    return {
+      arrowImage: '',
+      cv: ''
+    };
+  },
+  methods: {
+    updateArrowImage() {
+      this.arrowImage = document.body.dataset.bsTheme === 'dark' ? arrowWhite : arrowDark;
+    }
+  },
+  mounted() {
+    this.cv = cv;
+
+    // Initialisation de l'image au chargement du composant
+    this.updateArrowImage();
+
+    const mutationObserver = new MutationObserver(() => {
+      this.updateArrowImage();
+    });
+    mutationObserver.observe(document.body, { attributes: true });
+  }
+};
+</script>
+
+<style scoped>
+/* Style pour les flèches */
+.arrow {
+  position: absolute;
+  top: 20%;
+  font-size: 2rem;
+  color: #007bff; /* Couleur de la flèche */
+}
+
+.left-arrow {
+  left: -20%;
+}
+
+/* En dessous de 1800px de large, ne pas afficher la flèche */
+@media (max-width: 1800px) {
+  .arrow {
+    display: none;
+  }
+}
+</style>
