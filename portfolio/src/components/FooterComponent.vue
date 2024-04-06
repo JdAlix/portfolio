b84bfad2e49a7f71545092d5213256c4e1d3af37
Alix JEUDI--LEMOINE <alix.jeudi--lemoine@etu.uca.fr>
2024-04-06 17:21:33 +0200

    Ajout lien vers réseaux et mail

--- portfolio/src/components/FooterComponent.vue
+++ portfolio/src/components/FooterComponent.vue
@@ -1,7 +1,17 @@
 <template>
     <footer class="footer mt-auto py-3 bg-body-tertiary">
         <div class="container">
-        <span class="text-muted">© 2024 Mon Portfolio. Tous droits réservés.</span>
+            <span class="text-muted">© 2024 Alix JEUDI--LEMOINE. Tous droits réservés.</span>
+            
+            <a href="https://linkedin.alix-jdlm.fr" target="_blank">
+                <i class="bi bi-linkedin"></i>
+            </a>
+            <a href="https://github.com/JdAlix" target="_blank">
+                <i class="bi bi-github"></i>
+            </a>
+            <a href="mailto:alix.jeudi--lemoine@etu.uca.fr">
+                <i class="bi bi-envelope"></i>
+            </a>
         </div>
     </footer>
 </template>
@@ -11,3 +21,12 @@
         name: 'FooterComponent'
     }
 </script>
+
+
+<style>
+.footer a {
+    padding-left: 1%;
+    color: inherit;
+    text-decoration: none;
+}
+</style>
